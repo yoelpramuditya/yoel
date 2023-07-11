8561f11c48e662c86652704e55740224e57a9568
yoelpramuditya <139207880+yoelpramuditya@users.noreply.github.com>
2023-07-11 14:54:38 +0700

    Update yoel.py

--- yoel.py
+++ yoel.py
@@ -1,8 +1,20 @@
 import streamlit as st
 import pandas as pd
+col1, col2, col3 = st.columns(3)
+with col1:
+      st.write('')
+with col2:
+      st.title('WELCOME TO MY BIO')
+with col3:
+      st.write('')
+st.image('foto/9.jpg', width=200)
+st.title('PROFIL')
+st.info('NAMA : YOEL PRAMUDITYA')
+st.info('TTL : JAKARTA,21 JUNI 2003')
+st.info('AGAMA : KRISTEN')
+st.info('JENIS KELAMIN : LAKI-LAKI')
+st.info('PEKERJAAN : MAHASISWA')
 
-st.title('welcome')
-st.image('foto/9.jpg', width=300)
-st.title('YOEL PRAMUDITYA')
+st.title('
 
 
