31d83feb0814eb2fd0f8b627d552a62a886b2a9d
yoelpramuditya <139207880+yoelpramuditya@users.noreply.github.com>
2023-07-11 14:38:15 +0700

    Update yoel.py

--- yoel.py
+++ yoel.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-st title('welcome')
-st image('foto/9.jpg', width=300)
-st title('YOEL PRAMUDITYA')
+st.title('welcome')
+st.image('foto/9.jpg', width=300)
+st.title('YOEL PRAMUDITYA')
 
 
