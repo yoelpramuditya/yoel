9d8a83141210e821c774b53af95f799d7a6a87a4
yoelpramuditya <139207880+yoelpramuditya@users.noreply.github.com>
2023-07-11 15:18:14 +0700

    Update yoel.py

--- yoel.py
+++ yoel.py
@@ -17,10 +17,12 @@ st.info('PEKERJAAN : MAHASISWA')
 
 col1, col2, col3 = st.columns(3)
 with col1:
-      st.info('skills')
+      st.info('KEMAMPUAN')
       st.write('Mampu Menganalisis Data Kesehatan.\n\nMampu Mengcoding Diagnosa Penyakit.\n\nMampu Membuat Laporan Rekan Medis')
-      
-      
-with col2
-
+with col2:
+      st.info('RIWAYAT PENDIDIKAN')
+      st.write('SDN PETOJO UTARA 01 PAGI.\n\nSMPN 31 SEMARANG.\n\nSMKN 3 SEMARANG')
+with col3:
+      st.info('PENGALAMAN')
+      st.write('pernah bekerja dibagian rekam medis.\n\npernah bekerja di analisi data kesehatan')
 
