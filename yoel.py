42212e30fa0d34f50b37bb0a93bc6fdf7288efdb
yoelpramuditya <139207880+yoelpramuditya@users.noreply.github.com>
2023-07-11 16:43:58 +0700

    Update yoel.py

--- yoel.py
+++ yoel.py
@@ -8,7 +8,7 @@ with col2:
 with col3:
       st.write('')
 st.image('foto/9.jpg', width=200)
-st.title('TENTANG SAYA')
+st.title('**TENTANG SAYA**')
 st.info('**NAMA** : YOEL PRAMUDITYA')
 st.info('**TTL** : JAKARTA,21 JUNI 2003')
 st.info('**AGAMA** : KRISTEN')
@@ -29,12 +29,12 @@ with col3:
 st.info('**KENALI LEBIH JAUH**')
 col1, col2, col3 = st.columns(3)
 with col1:
-      st.info('TENTANG SAYA')
+      st.info('**TENTANG SAYA**')
       st.write('saya adalah seorang yang bergerak dibidang analisi data kesehatan dimana saya mempelajari semua yang berkaitan dengan data kesehatan.')
 with col2:
-      st.info('KONTAK PERSON')
+      st.info('**KONTAK PERSON**')
       st.write(':phone: call center [whatshap](https://wa.me/6283182905546)')
       st.write(':coffee: social media [instagram](https://instagram.com/pramuditya.yoel)')
 with col3:
-      st.info('SARAN DAN MASUKAN')
+      st.info('**SARAN DAN MASUKAN**')
       st.write(':mailbox: masukan dan saran [Disini](https://forms.gle/8AE7shhhMDdEh1iH9)')
