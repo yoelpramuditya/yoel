e102679d5fafca3e734ffea16c46b2bb0fa26316
yoelpramuditya <139207880+yoelpramuditya@users.noreply.github.com>
2023-07-11 15:38:44 +0700

    Update yoel.py

--- yoel.py
+++ yoel.py
@@ -25,5 +25,5 @@ with col2:
       st.write('• SDN PETOJO UTARA 01 PAGI.\n\n• SMPN 31 SEMARANG.\n\n• SMKN 3 SEMARANG')
 with col3:
       st.info('PENGALAMAN KERJA')
-      st.write('• Pernah Bekerja Dibagian Rekam Medis 2016-2018.\n\n• Pernah Bekerja Dianalisi Data Kesehatan 2019-2021.')
+      st.write('• Pernah Bekerja Dibagian Rekam Medis.\n\n• Pernah Bekerja Dianalisi Data Kesehatan.')
 
